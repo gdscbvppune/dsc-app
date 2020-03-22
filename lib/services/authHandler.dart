f17d016f27220ef7d2dd9ae8fa98fb3fb80bb560
Abhishek Dubey <abhi.dubey011999@gmail.com>
2020-03-22 23:27:09 +0530

    Auth Services and page switching are now functional
    Signed-off-by: Abhishek Dubey <abhi.dubey011999@gmail.com>

--- lib/services/authHandler.dart
+++ lib/services/authHandler.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-//import 'pageHandler.dart';
-//import '../pages/splashPage.dart';
-//import '../pages/loginPage.dart';
+import 'pageHandler.dart';
+import '../pages/splashPage.dart';
+import '../pages/loginPage.dart';
 
 String name, email, imageUrl;
 
@@ -13,14 +13,14 @@ class AuthHandler extends StatelessWidget {
         stream: FirebaseAuth.instance.onAuthStateChanged,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting)
-            //return SplashPage();
+            return SplashPage();
           else if (snapshot.hasData && snapshot.data != null) {
             name = snapshot.data.displayName;
             email = snapshot.data.email;
             imageUrl = snapshot.data.photoUrl;
-            //return PageHandler();
+            return PageHandler();
           } else
-            //return LoginPage();
+            return LoginPage();
         });
   }
 }
